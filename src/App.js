701a528f9bad91bf9207d9e4419ff1fbb4cd1e6d
Antonnio Jones <antonnioj@gmail.com>
2020-06-08 19:02:52 -0400

    update the nav added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,6 @@
 import React, {useState} from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-/*
- *The side bar navigation 
-*/
-const SideNav = () =>{
-
+import './main.css'
+// a const array of us states
   const US_STATES = ["Alaska",
                   "Alabama",
                   "Arkansas",
@@ -63,50 +57,82 @@ const SideNav = () =>{
                   "West Virginia",
                   "Wyoming"]
 
-  const EDUCATION_LEVEL = ["High School","Bacholr","Masters","Doctorial"];
+  //an array of education levels
+  const EDUCATION_LEVEL = ["K-12","bachelor","Masters","Doctorial"];
+  
 
+const ListOfUSStates = (props) =>{
   let listOfStates = US_STATES.map((value,index) => {
-    return <li className="usState"><button className="btn btn-link">{value}</button></li>
+    return <li className="usState" key={index}><button className="btn btn-link" onClick={() => props.setStateName(value)}>{value}</button></li>
   })
 
+  return(listOfStates)
 
+}
+
+const ListOfEducationLevels = (props) =>{
   let educationLevel = EDUCATION_LEVEL.map((value,index) =>{
-  return <li className="educationLevel"><button className="btn btn-link">{value}</button></li>
+    return <li className="educationLevel" key={index}><button className="btn btn-link">{value}</button></li>
   })
 
+  return(educationLevel)
+}
+
+/*
+ *The side bar navigation 
+*/
+const SideNav = (props) =>{
+  
   return(
-    <div>
-      <h3>DashBoard</h3>
-      <ul className="nav flex-column fixed-left">
-        <li className="nav-item">
-          <h4  data-toggle="collapse" href="#statesList" role="button" aria-expanded="false" aria-controls="statesList">
+    <div className="col" id="sideNav">
+      <h3 className="border-bottom">DashBoard</h3>
+       <ul className="nav flex-column fixed-left ">
+        <li className="nav-item ">
+          <h4 className="dropdown-toggle" data-toggle="collapse" href="#statesList" role="button" aria-expanded="false" aria-controls="statesList">
             States
           </h4>
         </li>
-        <div class="collapse" id="statesList">
-            {listOfStates}
+        <div className="collapse" id="statesList">
+            <ListOfUSStates setStateName={(stateName) => props.setStateName(stateName)}/>
         </div>
         <li>
-          <h4 data-toggle="collapse" href="#EducationLevel" role="button" aria-expanded="false" aria-controls="EducationLevel">
+          <h4 className="dropdown-toggle" data-toggle="collapse" href="#EducationLevel" role="button" aria-expanded="false" aria-controls="EducationLevel">
             Education Level
           </h4>
         </li>
-        <div class="collapse" id="EducationLevel">
-            {educationLevel}
+        <div className="collapse" id="EducationLevel">
+           
         </div>
       </ul>
+      
     </div>
-
-    //controls the collapse
-    
   )
 }
 
-function App() {
+const DataSection = (props) =>{
   return (
-    <div className="App">
+    <div className="col" id="DataSection">
+      <h1>{props.title}</h1>
+    </div>
+  )
+}
 
-      <SideNav></SideNav>
+const DataCard = () =>{
+
+}
+
+/**
+ * root component
+ */
+function App() {
+  let [stateName,setStateName] = useState('Georgia')
+  
+  return (
+    <div className="App container-fluid">
+      <div id="row">
+        <SideNav setStateName={(value) => setStateName(value)}></SideNav>
+        <DataSection title={stateName}></DataSection>
+      </div>
     </div>
   );
 }
