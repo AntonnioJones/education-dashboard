8e9954478781a4b1b76b45c39410a81cfcca788a
Antonnio Jones <antonnioj@gmail.com>
2020-06-12 22:31:10 -0400

    Updates the ui

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React, {useState} from 'react';
-import {Container,Row, Col, Button, NavDropdown} from 'react-bootstrap'
+import {Container,Row, Col, Button, NavDropdown,NavLink, NavItem, CardColumns, Dropdown, Navbar, NavbarBrand, Modal} from 'react-bootstrap'
 import Nav from 'react-bootstrap/Nav'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import DropdownItem from 'react-bootstrap/DropdownItem';
+import Card from 'react-bootstrap/Card'
 
 import './main.css'
+import NavbarCollapse from 'react-bootstrap/NavbarCollapse';
 
 // a const array of us states
   const US_STATES = ["Alaska",
                   "Alabama",
                   "Arkansas",
-                  "American Samoa",
                   "Arizona",
                   "California",
                   "Colorado",
@@ -37,7 +38,7 @@ import './main.css'
                   "Mississippi",
                   "Montana",
                   "North Carolina",
-                  " North Dakota",
+                  "North Dakota",
                   "Nebraska",
                   "New Hampshire",
                   "New Jersey",
@@ -48,7 +49,6 @@ import './main.css'
                   "Oklahoma",
                   "Oregon",
                   "Pennsylvania",
-                  "Puerto Rico",
                   "Rhode Island",
                   "South Carolina",
                   "South Dakota",
@@ -56,7 +56,6 @@ import './main.css'
                   "Texas",
                   "Utah",
                   "Virginia",
-                  "Virgin Islands",
                   "Vermont",
                   "Washington",
                   "Wisconsin",
@@ -68,88 +67,158 @@ import './main.css'
   
 
 const ListOfUSStates = (props) =>{
+  //a state variable that controls where the modal is visible
+  const [visible, setVisible] = useState(false);
+
+  //opens and closes the model
+  const openMenu = () => setVisible(true)
+  const closeMenu = () => setVisible(false);
+
+  /*calls the prop function that updates the US state
+  * then it closes the modal
+  */
+  const updateUSState = (value) =>{
+    props.setUSState(value);
+    closeMenu();
+  }
+
   let listOfStates = US_STATES.map((value,index) => {
     return(
-      <DropdownItem key={index}>
-        <Button onClick={() => props.setStateName(value)}>
+        <Button variant="link" key ={index} onClick={() => updateUSState(value)}>
           {value}
         </Button>
-      </DropdownItem>
     )
   })
 
   return(
-    <NavDropdown title={"States"}>
-        {listOfStates}
-    </NavDropdown>
+    <>
+      <NavItem as={Button} variant="link" onClick={openMenu}>
+        States
+      </NavItem>
+
+      <Modal show={visible} onHide={closeMenu}>
+        <Modal.Header>
+          <Modal.Title>Choose a State</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          {listOfStates}
+        </Modal.Body>
+      </Modal>
+    </>
   )
 
 }
 
 const ListOfEducationLevels = (props) => {
+  //a state variable that controls where the modal is visible
+  const [visible, setVisible] = useState(false);
+
+  //opens and closes the model
+  const openMenu = () => setVisible(true)
+  const closeMenu = () => setVisible(false);
+
+  /*calls the prop function that updates the education level
+  * then it closes the modal
+  */
+  const updateEducation = (value) =>{
+    props.setEducationLevel(value);
+    closeMenu();
+  }
+
+  //maps out an array of education levels to buttons
   let educationLevel = EDUCATION_LEVEL.map((value,index) => {
     return (
-      <DropdownItem key={index}>
-        <Button onClick={() => props.setEducationLevel(value)}>
-          {value}
-        </Button>
-      </DropdownItem>
+      <Button variant="link" key={index} onClick={() => updateEducation(value)}>
+        {value}
+      </Button>
     )
   })
 
   return(
-    <NavDropdown title={"Education Level"} >
-      {educationLevel}
-    </NavDropdown>
+    <>
+      <NavItem as={Button} variant="link" onClick={openMenu}>
+        Education
+      </NavItem>
+
+      <Modal show={visible} onHide={closeMenu}>
+        <Modal.Header>
+          <Modal.Title>Choose an Education Level</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          {educationLevel}
+        </Modal.Body>
+      </Modal>
+    </>
   )
 }
 
 /*
  *The side bar navigation 
 */
-const SideNav = (props) =>{
+const NavigationBar = (props) =>{
   
   return(
-    <Col xs={6} md={4}>
-      <h3 id="SideNav">DashBoard</h3>
-      <Nav className="flex-column">
-        <ListOfUSStates setStateName={(value) => props.setStateName(value)}></ListOfUSStates>
+    <Navbar bg="dark" variant="dark" expand="md" fixed="top" id="NavBar">
+      <NavbarBrand>DashBoard</NavbarBrand>
+      <Navbar.Toggle aria-controls="collapseLinks" />
+      <NavbarCollapse id="collapseLinks">
+      <Nav>
+        <ListOfUSStates setUSState={(value) => props.setUSState(value)}></ListOfUSStates>
         <ListOfEducationLevels setEducationLevel={(value) => props.setEducationLevel(value)}></ListOfEducationLevels>
       </Nav>
-    </Col>
+      </NavbarCollapse>
+    </Navbar>
   )
 }
 
 const DataSection = (props) =>{
   return (
-    <Col xs={12} md={8}>
-      <h1>{props.title}</h1>
-      <h6>{props.level}</h6>
-    </Col>
+    <section  id="DataSection">
+      <header id="DataHeader">
+        <h1>{props.title}</h1>
+        <h6>{props.level}</h6>
+      </header>
+      <section id="DataCards">
+        <DataCard title={"Total Students"} value={70000}></DataCard>
+        <DataCard title={"Prekindergarten"} value={1000}></DataCard>
+        <DataCard title={"kindergarten"} value={1000}></DataCard>
+        <DataCard title={"Grades 1-8"} value={1000}></DataCard>
+        <DataCard title={"High Schoolers"} value={1000}></DataCard>
+        <DataCard title={"Dropout Rate"} value={1000}></DataCard>
+        <DataCard title={"Graduation Rate"} value={1000}></DataCard>
+      </section>
+    </section>
   )
 }
 
-const DataCard = () =>{
-
+const DataCard = (props) =>{
+  return(
+    <Card border="dark" bg="info" className="text-center DataCard">
+      <Card.Body>
+        <Card.Header as="h5">{props.title}</Card.Header>
+        <Card.Text>
+          {props.value}
+        </Card.Text>
+      </Card.Body>
+    </Card>
+  )
 }
 
 /**
  * root component
  */
 function App() {
-  let [stateName,setStateName] = useState('Georgia');
+  let [USState,setUSState] = useState('Georgia');
   let [educationlevel,setEducationLevel] = useState('k-12')
   
   return (
-    <Container fluid>
-      <Row>
-        <SideNav 
-          setStateName={(value) => setStateName(value)}
+    <Container id="App" fluid>
+        <NavigationBar 
+          setUSState={(value) => setUSState(value)}
           setEducationLevel={(value) => setEducationLevel(value)}
           >
-        </SideNav>
-        <DataSection title={stateName} level={educationlevel}></DataSection>
-      </Row>
+        </NavigationBar>
+        <DataSection title={USState} level={educationlevel}></DataSection>
     </Container>
   );
 }
