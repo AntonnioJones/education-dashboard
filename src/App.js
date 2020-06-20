9610aca5f03548096c7e4cdb484bd340dc18621b
Antonnio Jones <antonnioj@gmail.com>
2020-06-20 16:07:29 -0400

    Added a national button to show all the totals

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect} from 'react';
-import {Container, Button, NavItem, Navbar, NavbarBrand, Modal} from 'react-bootstrap'
+import {Container, Button, NavItem, Navbar, NavbarBrand, Modal, NavLink} from 'react-bootstrap'
 import Nav from 'react-bootstrap/Nav'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Card from 'react-bootstrap/Card'
@@ -80,6 +80,7 @@ const ListOfUSStates = (props) =>{
   const updateUSState = (value) =>{
     props.setUSState(value);
     getHighSchoolData(value.toLowerCase(),props.setApiData);
+    props.showNational(false);
     closeMenu();
   }
 
@@ -154,6 +155,18 @@ const ListOfEducationLevels = (props) => {
   )
 }
 
+const NationalDataLink = (props) =>{
+  const updateNational =() =>{
+    getHighSchoolData('total',props.setApiData);
+    props.showNational(true);
+  }
+  return (
+    <NavItem as={Button} variant="link" onClick={updateNational} >
+      National
+    </NavItem>
+  )
+}
+
 /*
  *The side bar navigation 
 */
@@ -165,16 +178,35 @@ const NavigationBar = (props) =>{
       <Navbar.Toggle aria-controls="collapseLinks" />
       <NavbarCollapse id="collapseLinks">
       <Nav>
-        <ListOfUSStates 
+        <ListOfUSStates
+          showNational={(value) => props.showNational(value)} 
           setUSState={(value) => props.setUSState(value)}
           setApiData={(value) => props.setApiData(value)}
         ></ListOfUSStates>
         <ListOfEducationLevels 
           setEducationLevel={(value) => props.setEducationLevel(value)}
         ></ListOfEducationLevels>
+        <NationalDataLink
+          showNational={(value) => props.showNational(value)}
+          setApiData={(value) => props.setApiData(value)}
+        ></NationalDataLink>
       </Nav>
       </NavbarCollapse>
     </Navbar>
+  )
+}
+
+const SectionlData = (props) =>{
+  return(
+    <section id="DataCards">
+    <DataCard title={"Total Students"} value={props.apiData.TotalStudents}></DataCard>
+    <DataCard title={"Prekindergarten"} value={props.apiData.Prekindergarten}></DataCard>
+    <DataCard title={"kindergarten"} value={props.apiData.Kindergarten}></DataCard>
+    <DataCard title={"Grades 1-8"} value={props.apiData.PreHighSchool}></DataCard>
+    <DataCard title={"High School"} value={props.apiData.HighSchool}></DataCard>
+    <DataCard title={"Dropout Rate"} value={props.apiData.DropoutRate}></DataCard>
+    <DataCard title={"Graduation Rate"} value={props.apiData.GraduationRate}></DataCard>
+  </section>
   )
 }
 
@@ -185,15 +217,8 @@ const DataSection = (props) =>{
         <h1>{props.title}</h1>
         <h6>{props.level}</h6>
       </header>
-      <section id="DataCards">
-        <DataCard title={"Total Students"} value={props.apiData.TotalStudents}></DataCard>
-        <DataCard title={"Prekindergarten"} value={props.apiData.Prekindergarten}></DataCard>
-        <DataCard title={"kindergarten"} value={props.apiData.Kindergarten}></DataCard>
-        <DataCard title={"Grades 1-8"} value={props.apiData.PreHighSchool}></DataCard>
-        <DataCard title={"High School"} value={props.apiData.HighSchool}></DataCard>
-        <DataCard title={"Dropout Rate"} value={props.apiData.DropoutRate}></DataCard>
-        <DataCard title={"Graduation Rate"} value={props.apiData.GraduationRate}></DataCard>
-      </section>
+
+      <SectionlData apiData={props.apiData}/>
     </section>
   )
 }
@@ -223,7 +248,6 @@ const getHighSchoolData = (USState, callback) =>{
       var status = xhr.status;
       if (status === 0 || (status >= 200 && status < 400)) {
         // The request has been completed successfully
-        console.log(JSON.parse(xhr.responseText));
         callback(JSON.parse(xhr.responseText));
         return(xhr.responseText);
       } else {
@@ -236,30 +260,50 @@ const getHighSchoolData = (USState, callback) =>{
 }
 
 
+const RenderData = (props) =>{
+  if(props.national){
+    return(
+      <DataSection 
+      title={'National'} 
+      level={props.educationLevel} 
+      apiData={props.apiData}>
+    </DataSection>
+    )
+  }else{
+    return (
+      <DataSection 
+        title={props.USState} 
+        level={props.educationLevel} 
+        apiData={props.apiData}>
+      </DataSection>
+    );
+  }
+}
+
 /**
  * root component
  */
 function App() {
-  let [USState,setUSState] = useState('Alaska');
-  let [educationlevel,setEducationLevel] = useState('k-12')
+  let [USState,setUSState] = useState('total');
+  let [national,showNational] = useState(true);
+  let [educationLevel,setEducationLevel] = useState('k-12')
   let [apiData,setApiData] = useState({});
 
-  useEffect(() => {
-    //getHighSchoolData(USState.toLowerCase(),(value) => {setApiData(value)});
-  })
-  
-  
+  useEffect(()=>{
+    getHighSchoolData('total',setApiData)
+  }, [])
   return (
     <Container id="App" fluid>
-        <NavigationBar 
-          setUSState={(value) => setUSState(value)}
-          setEducationLevel={(value) => setEducationLevel(value)}
-          setApiData = {(value) => setApiData(value)}
-          >
-        </NavigationBar>
-        <DataSection title={USState} level={educationlevel} apiData={apiData}></DataSection>
+      <NavigationBar 
+        showNational={(value) => showNational(value)}
+        setUSState={(value) => setUSState(value)}
+        setEducationLevel={(value) => setEducationLevel(value)}
+        setApiData = {(value) => setApiData(value)}
+        >
+      </NavigationBar>
+      <RenderData national={national} USState={USState} educationLevel={educationLevel} apiData={apiData} />
     </Container>
-  );
+  )
 }
 
 export default App;
